e927faf066dee10a825d29e4cc79961dbca7eec4
Mehmet Pekdemir <mehmet.pekdemir@finartz.com>
2021-04-17 19:03:20 +0300

    app.

---
+++ src/pages/App/App.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello world !</h3>
+      </div>
+    );
+  }
+}
