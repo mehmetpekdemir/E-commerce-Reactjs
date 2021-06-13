cd2d3ff5d858b6764a4406cd24887ad729e21990
Mehmet Pekdemir <mehmet.pekdemir@finartz.com>
2021-06-13 16:00:04 +0300

    added.

--- src/pages/PhonePriceList/PhonePriceListView.js
+++ src/pages/PhonePriceList/PhonePriceListView.js
@@ -1,29 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Select from 'react-select'
 
 const PhonePriceListView = (props) => {
+    const [selected, setSelected] = useState(null);
+
+    const options = [
+        { value: 'iphone7', label: 'Iphone 7' },
+        { value: 'iphone8', label: 'Iphone 8' },
+        { value: 'iphoneX', label: 'Iphone X' },
+
+        { value: 'samsungNote20', label: 'Samsung Galaxy Note 20' },
+        { value: 'samsungNote10', label: 'Samsung Galaxy Note 10' },
+        { value: 'samsungS10', label: 'Samsung GalaxayS10 Plus' },
+
+        { value: 'xiomiRedmi9', label: 'Xiomi Redmi 9' },
+        { value: 'xiomiRedmi10', label: 'Xiomi Redmi 10' },
+        { value: 'xiomiRedmi10Pro', label: 'Xiomi Redmi 10 Pro' }
+    ]
+
+
     const { products } = props;
 
     const calculateAverage = () => {
+        if (selected) {
+            let data = [];
+            data.push(products.filter((product) => ((product.productName === selected.selectedOption.value))));
 
-        let data = [];
-        data.push(products.filter((product) => ((product.productName === "iphone7"))));
+            let priceList = [0];
+            data.map((arr) => (
+                arr.map((price) => priceList.push(price.productPrice))
+            ));
+            let totalCount = priceList.length;
+            let totalPrices = priceList.reduce((a, v) => a + v);
 
-        let priceList = [0];
-        data.map((arr) => (
-            arr.map((price) => priceList.push(price.productPrice))
-        ));
-        let totalCount = priceList.length;
-        let totalPrices = priceList.reduce((a, v) => a + v);
+            let average = (totalPrices / (totalCount - 1));
+            return average;
+        }
 
-        let average = (totalPrices / totalCount);
-        return average;
     }
 
-    let average = calculateAverage();
+    const handleChange = (selectedOption) => {
+        setSelected({ selectedOption });
+    }
+    const average = calculateAverage();
 
     return (
         <>
-            <h3>Average price = {average}</h3>
+            <div><Select className="mt-4 col-md-6 col-offset-4"
+                options={options}
+                onChange={handleChange} />
+            </div>
+            <div><h3>Average price = {average}</h3></div>
         </>
 
     )
